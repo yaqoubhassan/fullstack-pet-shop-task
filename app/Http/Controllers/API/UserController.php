e5e1842863450dcc8102ecc5e0c2e6c4a40a646c
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-06-30 19:14:56 +0000

    Add user logout logic and remove redundant function

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -33,47 +33,47 @@ class UserController extends Controller
     }
 
     /**
-    * @OA\Schema(
-    *     schema="StoreUserRequest",
-    *     required={"name", "email", "password"},
-    *     @OA\Property(property="name", type="string"),
-    *     @OA\Property(property="email", type="string", format="email"),
-    *     @OA\Property(property="password", type="string", format="password"),
-    *     @OA\Property(property="avatar", type="string", format="binary"),
-    * )
-    */
+     * @OA\Schema(
+     *     schema="StoreUserRequest",
+     *     required={"name", "email", "password"},
+     *     @OA\Property(property="name", type="string"),
+     *     @OA\Property(property="email", type="string", format="email"),
+     *     @OA\Property(property="password", type="string", format="password"),
+     *     @OA\Property(property="avatar", type="string", format="binary"),
+     * )
+     */
 
     /**
-    * @OA\Post(
-    *     path="/api/v1/user/create",
-    *     tags={"User"},
-    *     summary="Create a User Account",
-    *     @OA\RequestBody(
-    *       @OA\JsonContent(),
-    *       @OA\MediaType(
-    *           mediaType="application/x-www-form-urlencoded",
-    *           @OA\Schema(
-    *               type="object",
-    *               required={"first_name", "last_name", "email", "password", "password_confirmation", "phone_number", "address"},
-    *               @OA\Property(property="first_name", type="string", description="User firstname", example="Yakubu"),
-    *               @OA\Property(property="last_name", type="string", description="User lastname", example="Alhassan"),
-    *               @OA\Property(property="email", type="string", format="email", description="User email", example="yakubu@test.com"),
-    *               @OA\Property(property="password", type="string", example="secret123", description="User password"),
-    *               @OA\Property(property="password_confirmation", example="secret123", description="User password"),
-    *               @OA\Property(property="avatar", type="string", description="Avatar image UUID", example=""),
-    *               @OA\Property(property="address", type="string", description="User main address", example="Accra, Ghana"),
-    *               @OA\Property(property="phone_number", type="string", description="User main phone number", example="+233244112288"),
-    *               @OA\Property(property="is_marketing", type="string", description="User marketing preferences", example=""),
-    *           ),
-    *       ),
-    *     ),
-    *     @OA\Response(response="201", description="OK", @OA\JsonContent()),
-    *     @OA\Response(response="401", description="Unauthorized", @OA\JsonContent()),
-    *     @OA\Response(response="404", description="Page Not Found", @OA\JsonContent()),
-    *     @OA\Response(response="422", description="Unprocessable Entity", @OA\JsonContent()),
-    *     @OA\Response(response="500", description="Internal server error", @OA\JsonContent())
-    * )
-    */
+     * @OA\Post(
+     *     path="/api/v1/user/create",
+     *     tags={"User"},
+     *     summary="Create a User Account",
+     *     @OA\RequestBody(
+     *       @OA\JsonContent(),
+     *       @OA\MediaType(
+     *           mediaType="application/x-www-form-urlencoded",
+     *           @OA\Schema(
+     *               type="object",
+     *               required={"first_name", "last_name", "email", "password", "password_confirmation", "phone_number", "address"},
+     *               @OA\Property(property="first_name", type="string", description="User firstname", example="Yakubu"),
+     *               @OA\Property(property="last_name", type="string", description="User lastname", example="Alhassan"),
+     *               @OA\Property(property="email", type="string", format="email", description="User email", example="yakubu@test.com"),
+     *               @OA\Property(property="password", type="string", example="secret123", description="User password"),
+     *               @OA\Property(property="password_confirmation", example="secret123", description="User password"),
+     *               @OA\Property(property="avatar", type="string", description="Avatar image UUID", example=""),
+     *               @OA\Property(property="address", type="string", description="User main address", example="Accra, Ghana"),
+     *               @OA\Property(property="phone_number", type="string", description="User main phone number", example="+233244112288"),
+     *               @OA\Property(property="is_marketing", type="string", description="User marketing preferences", example=""),
+     *           ),
+     *       ),
+     *     ),
+     *     @OA\Response(response="201", description="OK", @OA\JsonContent()),
+     *     @OA\Response(response="401", description="Unauthorized", @OA\JsonContent()),
+     *     @OA\Response(response="404", description="Page Not Found", @OA\JsonContent()),
+     *     @OA\Response(response="422", description="Unprocessable Entity", @OA\JsonContent()),
+     *     @OA\Response(response="500", description="Internal server error", @OA\JsonContent())
+     * )
+     */
     public function store(StoreUserRequest $request)
     {
         // Begin a transaction
@@ -94,8 +94,6 @@ class UserController extends Controller
 
             // Generate JWT token
             $token = $this->jwtService->generateToken($user);
-
-            $this->saveToken($user);
 
             // Commit the transaction
             DB::commit();
@@ -143,50 +141,50 @@ class UserController extends Controller
     }
 
     /**
-    * @OA\Schema(
-    *     schema="StoreUserRequest",
-    *     required={"name", "email", "password"},
-    *     @OA\Property(property="name", type="string"),
-    *     @OA\Property(property="email", type="string", format="email"),
-    *     @OA\Property(property="password", type="string", format="password"),
-    *     @OA\Property(property="avatar", type="string", format="binary"),
-    * )
-    */
+     * @OA\Schema(
+     *     schema="StoreUserRequest",
+     *     required={"name", "email", "password"},
+     *     @OA\Property(property="name", type="string"),
+     *     @OA\Property(property="email", type="string", format="email"),
+     *     @OA\Property(property="password", type="string", format="password"),
+     *     @OA\Property(property="avatar", type="string", format="binary"),
+     * )
+     */
 
     /**
-    * @OA\Post(
-    *     path="/api/v1/user/login",
-    *     tags={"User"},
-    *     summary="Login a User Account",
-    *     @OA\RequestBody(
-    *       @OA\JsonContent(),
-    *       @OA\MediaType(
-    *           mediaType="application/x-www-form-urlencoded",
-    *           @OA\Schema(
-    *               type="object",
-    *               required={"email", "password"},
-    *               @OA\Property(property="email", type="string", format="email", description="User email", example=""),
-    *               @OA\Property(property="password", type="string", example="", description="User password"),
-    *           ),
-    *       ),
-    *     ),
-    *     @OA\Response(response="200", description="OK", @OA\JsonContent()),
-    *     @OA\Response(response="401", description="Unauthorized", @OA\JsonContent()),
-    *     @OA\Response(response="404", description="Page Not Found", @OA\JsonContent()),
-    *     @OA\Response(response="422", description="Unprocessable Entity", @OA\JsonContent()),
-    *     @OA\Response(response="500", description="Internal server error", @OA\JsonContent())
-    * )
-    */
+     * @OA\Post(
+     *     path="/api/v1/user/login",
+     *     tags={"User"},
+     *     summary="Login a User Account",
+     *     @OA\RequestBody(
+     *       @OA\JsonContent(),
+     *       @OA\MediaType(
+     *           mediaType="application/x-www-form-urlencoded",
+     *           @OA\Schema(
+     *               type="object",
+     *               required={"email", "password"},
+     *               @OA\Property(property="email", type="string", format="email", description="User email", example=""),
+     *               @OA\Property(property="password", type="string", example="", description="User password"),
+     *           ),
+     *       ),
+     *     ),
+     *     @OA\Response(response="200", description="OK", @OA\JsonContent()),
+     *     @OA\Response(response="401", description="Unauthorized", @OA\JsonContent()),
+     *     @OA\Response(response="404", description="Page Not Found", @OA\JsonContent()),
+     *     @OA\Response(response="422", description="Unprocessable Entity", @OA\JsonContent()),
+     *     @OA\Response(response="500", description="Internal server error", @OA\JsonContent())
+     * )
+     */
     public function login(LoginRequest $request)
     {
         if (Auth::attempt($request->validated())) {
-            $user = User::where('id', Auth::user()->id)->first();
+            $user = User::find(Auth::user()->id);
             $token = $this->jwtService->generateToken($user);
 
             $response = [
                 'success' => 1,
                 'data' => [
-                  'token' => $token
+                    'token' => $token
                 ],
                 'error' => null,
                 'errors' => [],
@@ -196,6 +194,36 @@ class UserController extends Controller
         }
 
         return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/api/v1/user/logout",
+     *     tags={"User"},
+     *     summary="Logout a User Account",
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Response(response="200", description="OK", @OA\JsonContent()),
+     *     @OA\Response(response="401", description="Unauthorized", @OA\JsonContent()),
+     *     @OA\Response(response="404", description="Page Not Found", @OA\JsonContent()),
+     *     @OA\Response(response="422", description="Unprocessable Entity", @OA\JsonContent()),
+     *     @OA\Response(response="500", description="Internal server error", @OA\JsonContent())
+     * )
+     */
+    public function logout(Request $request)
+    {
+        $user = $request->attributes->get('user');
+        $this->jwtService->invalidateToken($user);
+        Auth::logout();
+
+        $response = [
+            'success' => 1,
+            'data' => [],
+            'error' => null,
+            'errors' => [],
+            'extra' => []
+        ];
+
+        return response()->json($response, 200);
     }
 
     /**
@@ -220,19 +248,5 @@ class UserController extends Controller
         ]);
 
         return $fileRecord->uuid;
-    }
-
-    private function saveToken($user)
-    {
-        JwtToken::create([
-            'user_id' => $user->id,
-            'unique_id' => (string) Str::uuid(),
-            'token_title' => 'User Auth Token',
-            'permissions' => json_encode(['*']),
-            'restrictions' => null,
-            'expires_at' => now()->addHours(1),
-            'last_used_at' => now(),
-            'refreshed_at' => now(),
-        ]);
     }
 }
