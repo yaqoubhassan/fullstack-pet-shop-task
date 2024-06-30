8242b54be12a8d9010ee6c3784ca6b983026d3a1
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-06-30 12:02:12 +0000

    Install and configure Swagger for API documentation
    - Add `darkaonline/l5-swagger` package for generating API documentation.
    
    - Publish Swagger configuration and assets.
    
    - Add api documentation for user creation endpoint

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -30,6 +30,49 @@ class UserController extends Controller
         //
     }
 
+    /**
+    * @OA\Schema(
+    *     schema="StoreUserRequest",
+    *     required={"name", "email", "password"},
+    *     @OA\Property(property="name", type="string"),
+    *     @OA\Property(property="email", type="string", format="email"),
+    *     @OA\Property(property="password", type="string", format="password"),
+    *     @OA\Property(property="avatar", type="string", format="binary"),
+    * )
+    */
+
+    /**
+    * @OA\Post(
+    *     path="/api/v1/user/create",
+    *     tags={"User"},
+    *     summary="Create a User Account",
+    *     @OA\RequestBody(
+    *       @OA\JsonContent(),
+    *       @OA\MediaType(
+    *           mediaType="application/x-www-form-urlencoded",
+    *           @OA\Schema(
+    *               type="object",
+    *               required={"first_name", "last_name", "email", "password", "password_confirmation", "phone_number", "address"},
+    *               @OA\Property(property="first_name", type="string", description="User firstname", example="Yakubu"),
+    *               @OA\Property(property="last_name", type="string", description="User lastname", example="Alhassan"),
+    *               @OA\Property(property="email", type="string", format="email", description="User email", example="yakubu@test.com"),
+    *               @OA\Property(property="password", type="string", example="secret123", description="User password"),
+    *               @OA\Property(property="password_confirmation", example="secret123", description="User password"),
+    *               @OA\Property(property="avatar", type="string", description="Avatar image UUID", example=""),
+    *               @OA\Property(property="address", type="string", description="User main address", example="Accra, Ghana"),
+    *               @OA\Property(property="phone_number", type="string", description="User main phone number", example="+233244112288"),
+    *               @OA\Property(property="is_marketing", type="string", description="User marketing preferences", example=""),
+    *           ),
+    *       ),
+    *     ),
+    *     @OA\Response(response="200", description="OK", @OA\JsonContent()),
+    *     @OA\Response(response="201", description="OK", @OA\JsonContent()),
+    *     @OA\Response(response="401", description="Unauthorized", @OA\JsonContent()),
+    *     @OA\Response(response="404", description="Page Not Found", @OA\JsonContent()),
+    *     @OA\Response(response="422", description="Unprocessable Entity", @OA\JsonContent()),
+    *     @OA\Response(response="500", description="Internal server error", @OA\JsonContent())
+    * )
+    */
     public function store(StoreUserRequest $request)
     {
         // Begin a transaction
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,19 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *    title="Pet Shop API - Swagger Documentation",
+ *    version="1.0.0",
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     securityScheme="bearerAuth",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+
+ */
 abstract class Controller
 {
     //
