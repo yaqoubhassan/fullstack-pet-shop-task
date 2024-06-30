efcedcdb4b970c21950d97656b87c327992f6ee0
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-06-30 11:37:50 +0000

    Implement user creation functionality with JWT authentication
    - Implement logic to handle user creation.
    
    - Integrate JWT token generation and saving functionality.
    
    - Create a custom request for validating user creation inputs.
    
    - Add `handleFileUpload` method to manage avatar uploads.
    
    - Utilize transactions to ensure data integrity during user creation.
    
    - Add dependency injection for JwtService.
    
    - Add the create user endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\UserController;
+
+Route::post('v1/user/create', [UserController::class, 'store'])->name('user.create');
