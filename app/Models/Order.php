7d18ff57c6f9a810ee4b575e5099f3ce01dffa92
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-01 22:34:29 +0000

    Update `Order` model
    - Add all fillable attributes
    
    - Define user, order status, and payment relationships

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'order_status_id',
+        'payment_id',
+        'uuid',
+        'products',
+        'address',
+        'delivery_fee',
+        'amount',
+        'shipped_at'
+    ];
+
+    protected $casts = [
+        'products' => 'array',
+        'address' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderStatus()
+    {
+        return $this->belongsTo(OrderStatus::class);
+    }
+
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class);
+    }
 }
