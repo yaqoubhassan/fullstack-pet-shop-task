963909dba234bde7c80ebdfd28b82f5130b67c7a
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-02 11:45:43 +0000

    Add function for filtering and sorting categories when fetching all categories
    - Can sort by newest and oldest based on time created
    
    - Can list categories in alphabetical order based on title

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 
 class Category extends Model
 {
@@ -14,4 +15,32 @@ class Category extends Model
         'title',
         'slug'
     ];
+
+    /**
+     * Scope a query to apply filters and sorting.
+     *
+     * @param Builder $query
+     * @param array $filters
+     * @return Builder
+     */
+    public function scopeFilterAndSort(Builder $query, array $filters): Builder
+    {
+        if (isset($filters['sortBy'])) {
+            if ($filters['sortBy'] === 'newest') {
+                $query->orderBy('created_at', 'desc');
+            } elseif ($filters['sortBy'] === 'oldest') {
+                $query->orderBy('created_at', 'asc');
+            }
+        }
+
+        if (isset($filters['desc'])) {
+            if ($filters['desc'] === true || $filters['desc'] === 'true') {
+                $query->orderBy('title', 'desc');
+            } elseif ($filters['desc'] === false || $filters['desc'] === 'false') {
+                $query->orderBy('title', 'asc');
+            }
+        }
+
+        return $query;
+    }
 }
