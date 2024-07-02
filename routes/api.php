fda86589618dc921054622605fc31e2d06d95e2b
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-02 09:06:18 +0000

    Add category creation route

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\CategoryController;
 
-Route::prefix('v1/user')->group(function () {
-    Route::post('create', [UserController::class, 'store'])->name('user.create');
-    Route::post('login', [UserController::class, 'login'])->name('user.login');
-    Route::get('logout', [UserController::class, 'logout'])->middleware('jwt.auth')->name('user.logout');
-    Route::post('forgot-password', [UserController::class, 'forgotPassword'])->name('user.forgot-password');
-    Route::post('reset-password', [UserController::class, 'resetPassword'])->name('password.reset');
+Route::prefix('v1')->group(function () {
+    Route::prefix('user')->group(function () {
+        Route::post('create', [UserController::class, 'store'])->name('user.create');
+        Route::post('login', [UserController::class, 'login'])->name('user.login');
+        Route::get('logout', [UserController::class, 'logout'])->middleware('jwt.auth')->name('user.logout');
+        Route::post('forgot-password', [UserController::class, 'forgotPassword'])->name('user.forgot-password');
+        Route::post('reset-password', [UserController::class, 'resetPassword'])->name('password.reset');
 
-    Route::middleware('jwt.auth')->group(function () {
-        Route::get('/', [UserController::class, 'index'])->name('user.index');
-        Route::put('edit', [UserController::class, 'update'])->name('user.update');
-        Route::delete('/', [UserController::class, 'destroy'])->name('user.delete');
+        Route::middleware('jwt.auth')->group(function () {
+            Route::get('/', [UserController::class, 'index'])->name('user.index');
+            Route::put('edit', [UserController::class, 'update'])->name('user.update');
+            Route::delete('/', [UserController::class, 'destroy'])->name('user.delete');
+        });
+    });
+
+    //Category
+    Route::prefix('category')->middleware('jwt.auth')->group(function () {
+        Route::post('create', [CategoryController::class, 'store'])->name('category.create');
     });
 });
