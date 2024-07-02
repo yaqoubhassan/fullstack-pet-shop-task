ff07fb58a7375c9c2da48ce4b81dcbda6a5dc293
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-02 11:41:56 +0000

    Update `Categorytest`
    - Add all possible tests for listing all categories

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -59,7 +59,7 @@ class CategoryTest extends TestCase
         $response = $this->postJson(route('category.create'), [], $this->headers);
 
         $response->assertStatus(422)
-                 ->assertJsonValidationErrors(['title']);
+            ->assertJsonValidationErrors(['title']);
     }
 
     public function testCategoryAlreadyExists()
@@ -67,26 +67,123 @@ class CategoryTest extends TestCase
         $title = $this->faker->word;
 
         Category::create([
-           'title' => $title,
-           'uuid' => (string) Str::uuid(),
-           'slug' => Str::slug($title)
+            'title' => $title,
+            'uuid' => (string) Str::uuid(),
+            'slug' => Str::slug($title)
         ]);
 
         $response = $this->postJson(route('category.create'), [
-           'title' => $title
+            'title' => $title
         ], $this->headers);
 
         $response->assertStatus(201)
-                ->assertJson([
-                    'success' => 1,
-                    'data' => [
-                        'uuid' => true
-                    ],
-                    'error' => null,
-                    'errors' => [],
-                    'extra' => []
-                ]);
+            ->assertJson([
+                'success' => 1,
+                'data' => [
+                    'uuid' => true
+                ],
+                'error' => null,
+                'errors' => [],
+                'extra' => []
+            ]);
 
         $this->assertCount(1, Category::where('title', $title)->get());
+    }
+
+    public function testFetchFirstPageWithDefaultSettings()
+    {
+        Category::factory()->count(20)->create();
+        $response = $this->json('GET', route('category.list'), [], $this->headers);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => ['uuid', 'title', 'slug', 'created_at', 'updated_at']
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $this->assertCount(10, $response->json('data'));
+    }
+
+    public function testFetchCategoriesSortedByNewest()
+    {
+        Category::factory()->count(20)->create();
+        $response = $this->json('GET', route('category.list', ['sortBy' => 'newest']), [], $this->headers);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => ['uuid', 'title', 'slug', 'created_at', 'updated_at']
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $categories = Category::orderBy('created_at', 'desc')->take(10)->get();
+        $responseCategories = collect($response->json('data'));
+
+        $this->assertEquals($categories->pluck('uuid'), $responseCategories->pluck('uuid'));
+    }
+
+    public function testFetchCategoriesSortedByOldest()
+    {
+        Category::factory()->count(20)->create();
+        $response = $this->json('GET', route('category.list', ['sortBy' => 'oldest']), [], $this->headers);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => ['uuid', 'title', 'slug', 'created_at', 'updated_at']
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $categories = Category::orderBy('created_at', 'asc')->take(10)->get();
+        $responseCategories = collect($response->json('data'));
+
+        $this->assertEquals($categories->pluck('uuid'), $responseCategories->pluck('uuid'));
+    }
+
+    public function testFetchCategoriesSortedByTitleDescending()
+    {
+        Category::factory()->count(20)->create();
+        $response = $this->json('GET', route('category.list', ['desc' => 'true']), [], $this->headers);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => ['uuid', 'title', 'slug', 'created_at', 'updated_at']
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $categories = Category::orderBy('title', 'desc')->take(10)->get();
+        $responseCategories = collect($response->json('data'));
+
+        $this->assertEquals($categories->pluck('uuid'), $responseCategories->pluck('uuid'));
+    }
+
+    public function testFetchCategoriesSortedByTitleAscending()
+    {
+        Category::factory()->count(20)->create();
+        $response = $this->json('GET', route('category.list', ['desc' => 'false']), [], $this->headers);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => ['uuid', 'title', 'slug', 'created_at', 'updated_at']
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $categories = Category::orderBy('title', 'asc')->take(10)->get();
+        $responseCategories = collect($response->json('data'));
+
+        $this->assertEquals($categories->pluck('uuid'), $responseCategories->pluck('uuid'));
     }
 }
