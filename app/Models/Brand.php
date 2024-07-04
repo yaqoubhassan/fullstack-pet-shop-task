140153bdb0ddf6d489394fa4dab19632f38a9782
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-04 20:48:18 +0000

    Update `Brand` model
    - Add filter and sort method for filtering requests when listing brands

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 
 class Brand extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['uuid', 'title', 'slug'];
+
+    public function scopeFilterAndSort(Builder $query, array $filters): Builder
+    {
+        if (isset($filters['sortBy'])) {
+            if ($filters['sortBy'] === 'newest') {
+                $query->orderBy('created_at', 'desc');
+            } elseif ($filters['sortBy'] === 'oldest') {
+                $query->orderBy('created_at', 'asc');
+            }
+        }
+
+        if (isset($filters['desc'])) {
+            if ($filters['desc'] === true || $filters['desc'] === 'true') {
+                $query->orderBy('title', 'desc');
+            } elseif ($filters['desc'] === false || $filters['desc'] === 'false') {
+                $query->orderBy('title', 'asc');
+            }
+        }
+
+        return $query;
+    }
 }
