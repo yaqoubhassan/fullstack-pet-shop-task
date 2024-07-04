569f20468922531393e437dd4bb6abd9b53a2ae5
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-04 00:13:12 +0000

    Add tests for listing users

--- tests/Feature/Admin/UserTest.php
+++ tests/Feature/Admin/UserTest.php
@@ -20,11 +20,23 @@ class UserTest extends TestCase
     use RefreshDatabase;
 
     protected $jwtService;
+    protected $user;
+    protected $token;
+    protected $headers;
 
     public function setUp(): void
     {
         parent::setUp();
         $this->jwtService = new JwtService();
+        $this->user = User::factory()->create([
+            'is_admin' => true
+        ]);
+
+        $this->token = $this->jwtService->generateToken($this->user);
+
+        $this->headers = [
+            'Authorization' => 'Bearer ' . $this->token,
+        ];
     }
 
     public function testAdminUserCreation()
@@ -89,7 +101,7 @@ class UserTest extends TestCase
             'password' => 'password123',
             'password_confirmation' => 'password123',
             'address' => $this->faker->address,
-            'phone_number' => $this->faker->phoneNumber,
+            'phone_number' => substr($this->faker->phoneNumber, 0, 15)
         ];
 
         $userRepositoryMock = Mockery::mock(UserRepository::class);
@@ -102,5 +114,417 @@ class UserTest extends TestCase
             ->assertJsonStructure([
                 'error',
             ]);
+    }
+
+    public function testAdminCanListAllUsers()
+    {
+        User::factory()->count(20)->create();
+        $response = $this->json('GET', route('admin.user.list'), [], $this->headers);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'first_name',
+                        'last_name',
+                        'email',
+                        'avatar',
+                        'address',
+                        'phone_number',
+                        'is_marketing',
+                        'created_at',
+                        'updated_at'
+                    ]
+                ],
+                'links',
+                'meta'
+            ]);
+    }
+
+    public function testOnlyUserWithAdminAccountCanListAllUsers()
+    {
+        $admin = User::factory()->create([
+            'is_admin' => false
+        ]);
+
+        User::factory()->count(20)->create();
+
+        $token = $this->jwtService->generateToken($admin);
+
+        $headers = [
+            'Authorization' => 'Bearer ' . $token
+        ];
+        $response = $this->json('GET', route('admin.user.list'), [], $headers);
+
+        $response->assertStatus(422)
+            ->assertJson([
+                'success' => 0,
+                'data' => [],
+                'error' => 'Unauthorized: Not enough privileges',
+                'errors' => [],
+                'trace' => []
+            ]);
+    }
+
+    public function testCheckForUnauthenticatedUser()
+    {
+        User::factory()->count(20)->create();
+
+        $response = $this->json('GET', route('admin.user.list'));
+
+        $response->assertStatus(401)
+            ->assertJson([
+                'success' => 0,
+                'data' => [],
+                'error' => 'Unauthenticated',
+                'errors' => [],
+                'trace' => []
+            ]);
+    }
+
+    public function testFetchUsersSortedByNewest()
+    {
+        $users = User::factory()->count(20)->create()->each(function ($user, $index) {
+            $user->created_at = now()->subSeconds($index);
+            $user->save();
+        });
+        $response = $this->json('GET', route('admin.user.list', ['sortBy' => 'newest']), [], $this->headers);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'first_name',
+                        'last_name',
+                        'email',
+                        'avatar',
+                        'address',
+                        'phone_number',
+                        'is_marketing',
+                        'created_at',
+                        'updated_at'
+                    ]
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $users = User::where('is_admin', false)->orderBy('created_at', 'desc')->take(10)->get();
+        $responseUsers = collect($response->json('data'));
+
+        $this->assertEquals($users->pluck('uuid')->toArray(), $responseUsers->pluck('uuid')->toArray());
+    }
+
+    public function testFetchUsersSortedByOldest()
+    {
+        $users = User::factory()->count(20)->create()->each(function ($user, $index) {
+            $user->created_at = now()->subSeconds($index);
+            $user->save();
+        });
+        $response = $this->json('GET', route('admin.user.list', ['sortBy' => 'oldest']), [], $this->headers);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'first_name',
+                        'last_name',
+                        'email',
+                        'avatar',
+                        'address',
+                        'phone_number',
+                        'is_marketing',
+                        'created_at',
+                        'updated_at'
+                    ]
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $users = User::where('is_admin', false)->orderBy('created_at', 'asc')->take(10)->get();
+        $responseUsers = collect($response->json('data'));
+
+        $this->assertEquals($users->pluck('uuid')->toArray(), $responseUsers->pluck('uuid')->toArray());
+    }
+
+    public function testFetchUsersSortedByNameDescending()
+    {
+        $users = User::factory()->count(20)->create()->each(function ($user, $index) {
+            $user->created_at = now()->subSeconds($index);
+            $user->save();
+        });
+        $response = $this->json('GET', route('admin.user.list', ['desc' => 'true']), [], $this->headers);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'first_name',
+                        'last_name',
+                        'email',
+                        'avatar',
+                        'address',
+                        'phone_number',
+                        'is_marketing',
+                        'created_at',
+                        'updated_at'
+                    ]
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $users = User::where('is_admin', false)->orderBy('first_name', 'desc')->take(10)->get();
+        $responseUsers = collect($response->json('data'));
+
+        $this->assertEquals($users->pluck('uuid'), $responseUsers->pluck('uuid'));
+    }
+
+    public function testFetchUsersSortedByNameAscending()
+    {
+        $users = User::factory()->count(20)->create()->each(function ($user, $index) {
+            $user->created_at = now()->subSeconds($index);
+            $user->save();
+        });
+        $response = $this->json('GET', route('admin.user.list', ['desc' => 'false']), [], $this->headers);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'first_name',
+                        'last_name',
+                        'email',
+                        'avatar',
+                        'address',
+                        'phone_number',
+                        'is_marketing',
+                        'created_at',
+                        'updated_at'
+                    ]
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $users = User::where('is_admin', false)->orderBy('first_name', 'asc')->take(10)->get();
+        $responseUsers = collect($response->json('data'));
+
+        $this->assertEquals($users->pluck('uuid'), $responseUsers->pluck('uuid'));
+    }
+
+    public function testFetchUsersFilteredByCreatedAt()
+    {
+        User::factory()->count(10)->create()->each(function ($user, $index) {
+            $user->created_at = now()->subDays($index);
+            $user->save();
+        });
+
+        $filterDate = now()->subDays(5)->format('Y-m-d');
+        $response = $this->json('GET', route('admin.user.list', ['created_at' => $filterDate]), [], $this->headers);
+
+        $response->assertStatus(200)
+        ->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'uuid',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'avatar',
+                    'address',
+                    'phone_number',
+                    'is_marketing',
+                    'created_at',
+                    'updated_at'
+                ]
+            ],
+            'links',
+            'meta'
+        ]);
+
+        $users = User::where('is_admin', false)->whereDate('created_at', $filterDate)->get();
+        $responseUsers = collect($response->json('data'));
+
+        $this->assertCount($users->count(), $responseUsers);
+
+        $this->assertSame($users->pluck('uuid')->toArray(), $responseUsers->pluck('uuid')->toArray());
+
+        $users->each(function ($user) use ($response) {
+            $response->assertJsonFragment(['uuid' => $user->uuid]);
+        });
+    }
+
+    public function testFetchUsersFilteredByIsMarketing()
+    {
+        User::factory()->count(5)->create(['is_marketing' => true]);
+
+        User::factory()->count(5)->create(['is_marketing' => false]);
+
+        $response = $this->json('GET', route('admin.user.list', ['is_marketing' => true]), [], $this->headers);
+
+        $response->assertStatus(200)
+        ->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'uuid',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'avatar',
+                    'address',
+                    'phone_number',
+                    'is_marketing',
+                    'created_at',
+                    'updated_at'
+                ]
+            ],
+            'links',
+            'meta'
+        ]);
+
+        $users = User::where([['is_admin', false], ['is_marketing', true]])->get();
+        $responseUsers = collect($response->json('data'));
+
+        $this->assertCount($users->count(), $responseUsers);
+        $this->assertSame($users->pluck('uuid')->toArray(), $responseUsers->pluck('uuid')->toArray());
+
+        $users->each(function ($user) use ($response) {
+            $response->assertJsonFragment(['uuid' => $user->uuid]);
+        });
+    }
+
+    public function testFetchUsersFilteredByFirstName()
+    {
+        $firstName = 'John';
+        User::factory()->create(['first_name' => $firstName]);
+        User::factory()->count(9)->create();
+
+        $response = $this->json('GET', route('admin.user.list', ['first_name' => $firstName]), [], $this->headers);
+
+        $response->assertStatus(200)
+        ->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'uuid',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'avatar',
+                    'address',
+                    'phone_number',
+                    'is_marketing',
+                    'created_at',
+                    'updated_at'
+                ]
+            ],
+            'links',
+            'meta'
+        ]);
+
+        $responseUsers = collect($response->json('data'));
+        $this->assertTrue($responseUsers->pluck('first_name')->contains($firstName));
+    }
+
+    public function testFetchUsersFilteredByEmail()
+    {
+        $email = 'test@email.com';
+        User::factory()->create(['email' => $email]);
+        User::factory()->count(9)->create();
+
+        $response = $this->json('GET', route('admin.user.list', ['email' => $email]), [], $this->headers);
+
+        $response->assertStatus(200)
+        ->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'uuid',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'avatar',
+                    'address',
+                    'phone_number',
+                    'is_marketing',
+                    'created_at',
+                    'updated_at'
+                ]
+            ],
+            'links',
+            'meta'
+        ]);
+
+        $responseUsers = collect($response->json('data'));
+        $this->assertTrue($responseUsers->pluck('email')->contains($email));
+    }
+
+    public function testFetchUsersFilteredByPhoneNumber()
+    {
+        $phoneNumber = '0244004455';
+        User::factory()->create(['phone_number' => $phoneNumber]);
+        User::factory()->count(9)->create();
+
+        $response = $this->json('GET', route('admin.user.list', ['phone_number' => $phoneNumber]), [], $this->headers);
+
+        $response->assertStatus(200)
+        ->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'uuid',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'avatar',
+                    'address',
+                    'phone_number',
+                    'is_marketing',
+                    'created_at',
+                    'updated_at'
+                ]
+            ],
+            'links',
+            'meta'
+        ]);
+
+        $responseUsers = collect($response->json('data'));
+        $this->assertTrue($responseUsers->pluck('phone_number')->contains($phoneNumber));
+    }
+
+    public function testFetchUsersFilteredByAddress()
+    {
+        $address = 'Alajo, Accra';
+        User::factory()->create(['address' => $address]);
+        User::factory()->count(9)->create();
+
+        $response = $this->json('GET', route('admin.user.list', ['address' => $address]), [], $this->headers);
+
+        $response->assertStatus(200)
+        ->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'uuid',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'avatar',
+                    'address',
+                    'phone_number',
+                    'is_marketing',
+                    'created_at',
+                    'updated_at'
+                ]
+            ],
+            'links',
+            'meta'
+        ]);
+
+        $responseUsers = collect($response->json('data'));
+        $this->assertTrue($responseUsers->pluck('address')->contains($address));
     }
 }
