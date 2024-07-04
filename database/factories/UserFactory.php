d0d8b05a2f5e328bbdbc02219e8af76d1515b410
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-04 00:11:46 +0000

    Update `UserFactoy`

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('secret123'),
-            'phone_number' => $this->faker->phoneNumber(),
+            'phone_number' => substr($this->faker->phoneNumber(), 0, 15),
             'address' => $this->faker->address(),
             'avatar' => $this->faker->uuid(), // Simulating avatar as UUID
             'is_marketing' => $this->faker->boolean(),
