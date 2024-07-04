9e17c45f174d7c36c3919d21d1a97733617886af
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-04 21:20:18 +0000

    Create `BrandFactory`

---
+++ database/factories/BrandFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Brand>
+ */
+class BrandFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->sentence($nbWords = rand(1, 4));
+
+        return [
+            'title' => rtrim($title, '.'),
+            'uuid' => (string) Str::uuid(),
+            'slug' => Str::slug($title)
+        ];
+    }
+}
