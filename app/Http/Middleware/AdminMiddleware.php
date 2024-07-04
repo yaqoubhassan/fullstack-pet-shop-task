421916daabb31f7b4670703cf7705dea18738b62
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-04 00:10:43 +0000

    Update `AdminMiddleware`
    - Change error message

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
 use Closure;
 use App\Services\JwtService;
+use App\Models\User;
 
 class AdminMiddleware
 {
@@ -22,11 +23,33 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $user = auth()->user();
+        $token = $request->bearerToken();
 
-        if (!$user || !$user->is_admin) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        if (!$token || !$this->jwtService->validateToken($token)) {
+            return response()->json([
+                'success' => 0,
+                'data' => [],
+                'error' => 'Unauthenticated',
+                'errors' => [],
+                'trace' => []
+            ], 401);
         }
+
+        $userUuid = $this->jwtService->getUserUuidFromToken($token);
+        $user = User::where('uuid', $userUuid)->first();
+
+        if (!$user || !$user->is_admin) { // Ensure the user has the admin role
+            return response()->json([
+                'success' => 0,
+                'data' => [],
+                'error' => 'Unauthorized: Not enough privileges',
+                'errors' => [],
+                'trace' => []
+            ], 422);
+        }
+
+        auth()->setUser($user);
+        $request->attributes->set('user', $user);
 
         return $next($request);
     }
