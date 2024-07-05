27ec4cc2f1d8cdbd7a479b1fabe73eba5c2d4ae6
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-05 22:56:16 +0000

    Create file factory

---
+++ database/factories/FileFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\File>
+ */
+class FileFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $fileName = $this->faker->word;
+        $filePath = 'pet-shop/' . $this->faker->file('public/storage/pet-shop', storage_path('app/public'), false);
+        $fileUuid = Str::uuid()->toString();
+
+        return [
+            'uuid' => $fileUuid,
+            'name' => $fileName,
+            'path' => $filePath,
+            'size' => $this->faker->numberBetween(1000, 5000),
+            'type' => 'image/jpeg',
+        ];
+    }
+}
