478226605ca422ef2feb499b65dba132c9e8c0ac
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-06 19:54:40 +0000

    Add vue, vite and veutify configurations

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
-import './bootstrap';
+import { createApp } from "vue";
+import "./bootstrap";
+import App from "./components/app.vue";
+// Vuetify
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+import "vuetify/styles";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: "mdi",
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+});
+
+createApp(App).use(vuetify).mount("#app");
