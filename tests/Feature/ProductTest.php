0cca6eb108cd9ed469e68d30f62f6e65115c4f71
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-06 12:28:32 +0000

    Update `ProductTest`
    - Add all tests for filtering and sorting products

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Services\JwtService;
 use App\Models\User;
+use App\Models\Product;
 use App\Models\File;
 use App\Models\Category;
 use App\Models\Brand;
@@ -68,5 +69,253 @@ class ProductTest extends TestCase
             'title' => 'Test Product',
             'price' => 100.50
         ]);
+    }
+
+    public function testListAllProducts()
+    {
+        Product::factory()->count(10)->create();
+
+        $response = $this->json('GET', route('product.list'));
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'category_uuid',
+                        'title',
+                        'price',
+                        'description',
+                        'metadata',
+                        'created_at',
+                        'updated_at',
+                        'deleted_at',
+                        'category',
+                        'brand'
+                    ]
+                ],
+                'links',
+                'meta'
+            ])
+            ->assertJsonCount(10, 'data');
+    }
+
+    public function testFetchProductsSortedByNewest()
+    {
+        $products = Product::factory()->count(20)->create()->each(function ($product, $index) {
+            $product->created_at = now()->subSeconds($index);
+            $product->save();
+        });
+        $response = $this->json('GET', route('product.list', ['sortBy' => 'newest']));
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'category_uuid',
+                        'title',
+                        'price',
+                        'description',
+                        'metadata',
+                        'created_at',
+                        'updated_at',
+                        'deleted_at',
+                        'category',
+                        'brand'
+                    ]
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $products = Product::orderBy('created_at', 'desc')->take(10)->get();
+        $responseProducts = collect($response->json('data'));
+
+        $this->assertEquals($products->pluck('uuid')->toArray(), $responseProducts->pluck('uuid')->toArray());
+    }
+
+    public function testFetchProductsSortedByOldest()
+    {
+        $products = Product::factory()->count(20)->create()->each(function ($product, $index) {
+            $product->created_at = now()->subSeconds($index);
+            $product->save();
+        });
+        $response = $this->json('GET', route('product.list', ['sortBy' => 'oldest']));
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'category_uuid',
+                        'title',
+                        'price',
+                        'description',
+                        'metadata',
+                        'created_at',
+                        'updated_at',
+                        'deleted_at',
+                        'category',
+                        'brand'
+                    ]
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $products = Product::orderBy('created_at', 'asc')->take(10)->get();
+        $responseProducts = collect($response->json('data'));
+
+        $this->assertEquals($products->pluck('uuid')->toArray(), $responseProducts->pluck('uuid')->toArray());
+    }
+
+    public function testFetchProductsSortedByTitlteDescending()
+    {
+        $products = Product::factory()->count(20)->create()->each(function ($product, $index) {
+            $product->created_at = now()->subSeconds($index);
+            $product->save();
+        });
+        $response = $this->json('GET', route('product.list', ['desc' => 'true']));
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'category_uuid',
+                        'title',
+                        'price',
+                        'description',
+                        'metadata',
+                        'created_at',
+                        'updated_at',
+                        'deleted_at',
+                        'category',
+                        'brand'
+                    ]
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $products = Product::orderBy('title', 'desc')->take(10)->get();
+        $responseProducts = collect($response->json('data'));
+
+        $this->assertEquals($products->pluck('uuid'), $responseProducts->pluck('uuid'));
+    }
+
+    public function testFetchProductsSortedByTitleAscending()
+    {
+        $products = Product::factory()->count(20)->create()->each(function ($product, $index) {
+            $product->created_at = now()->subSeconds($index);
+            $product->save();
+        });
+        $response = $this->json('GET', route('product.list', ['desc' => 'false']));
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'category_uuid',
+                        'title',
+                        'price',
+                        'description',
+                        'metadata',
+                        'created_at',
+                        'updated_at',
+                        'deleted_at',
+                        'category',
+                        'brand'
+                    ]
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $products = Product::orderBy('title', 'asc')->take(10)->get();
+        $responseProducts = collect($response->json('data'));
+
+        $this->assertEquals($products->pluck('uuid'), $responseProducts->pluck('uuid'));
+    }
+
+    public function testSearchProductsByTitle()
+    {
+        $title = 'First Product';
+        Product::factory()->create(['title' => $title]);
+        Product::factory()->count(9)->create();
+
+        $response = $this->json('GET', route('product.list', ['title' => $title]));
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'category_uuid',
+                        'title',
+                        'price',
+                        'description',
+                        'metadata',
+                        'created_at',
+                        'updated_at',
+                        'deleted_at',
+                        'category',
+                        'brand'
+                    ]
+                ],
+                'links',
+                'meta'
+            ]);
+
+        $responseProducts = collect($response->json('data'));
+        $this->assertTrue($responseProducts->pluck('title')->contains($title));
+    }
+
+    public function testFetchProductsSortedByCategory()
+    {
+        $category = Category::factory()->create();
+        Product::factory()->count(7)->create();
+        Product::factory()->count(3)->create([
+            'category_uuid' => $category->uuid
+        ]);
+
+        $response = $this->json('GET', route('product.list'), ['category' => $category->uuid]);
+        $response->assertStatus(200)
+            ->assertJsonCount(3, 'data');
+    }
+
+    public function testFetchProductsFilterByPrice()
+    {
+        Product::factory()->count(7)->create([
+            'price' => $this->faker->randomFloat(2, 10, 100),
+        ]);
+        Product::factory()->count(3)->create([
+            'price' => 1000
+        ]);
+
+        $response = $this->json('GET', route('product.list'), ['price' => 1000]);
+        $response->assertStatus(200)
+            ->assertJsonCount(3, 'data');
+    }
+
+    public function testFetchProductsFilterByBrand()
+    {
+        $brand = Brand::factory()->create();
+        $file = File::factory()->create();
+        Product::factory()->count(7)->create();
+        Product::factory()->count(3)->create([
+            'metadata' => [
+                [
+                    'brand' => $brand->uuid,
+                    'image' => $file->uuid
+                ]
+            ]
+        ]);
+
+        $response = $this->json('GET', route('product.list'), ['brand' => $brand->uuid]);
+        $response->assertStatus(200)
+            ->assertJsonCount(3, 'data');
     }
 }
