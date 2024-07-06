bcb3f8e6fe0ddb01748b0dbc394f372d2b1991b8
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-06 07:55:33 +0000

    Add file -  product relationship

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
 
 class File extends Model
 {
     use HasFactory;
 
     protected $fillable = ['uuid', 'name', 'path', 'size', 'type'];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class, 'metadata->image', 'uuid');
+    }
 }
