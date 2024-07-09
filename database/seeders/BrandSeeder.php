28ea92d0bce63af4b19d2ba8470a4cafd5150210
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-07-09 08:07:19 +0000

    Create seeder for brands table

---
+++ database/seeders/BrandSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Brand;
+
+class BrandSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Brand::factory()->count(10)->create();
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,9 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $classes = [
+            AdminSeeder::class,
             OrderStatusSeeder::class,
             CategorySeeder::class,
-            AdminSeeder::class
+            BrandSeeder::class
         ];
         $this->call($classes);
     }
